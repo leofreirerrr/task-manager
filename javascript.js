803f780b58f70cd6d915489e6cc14c05dcf6de96
leofreirerrr <leofreirerr@gmail.com>
2024-12-12 11:04:17 -0300

    botão de excluir tudo e tratamenti de erro

--- javascript.js
+++ javascript.js
@@ -3,9 +3,17 @@ let taskList = document.querySelector(".task-list");
 let taskInput = document.querySelector("#type-new-task");
 const btnDelete = document.querySelector(".task-item i");
 let taskItem = document.querySelector(".task-item p");
-const bntDeleteAll = document
-
+const btnDeleteAll = document.querySelector("#btn-delete-all");
 let tasks = [];
+
+function verifyInput() {
+  if (taskInput.value.trim() == "") {
+    taskInput.style.border = "solid red";
+    taskInput.style.
+  } else {
+    return true;
+  }
+}
 
 function getTaskName() {
   const taskName = taskInput.value;
@@ -13,28 +21,29 @@ function getTaskName() {
   taskInput.value = "";
   return taskName;
 }
-taskList.addEventListener("click", (event)=> {
+taskList.addEventListener("click", (event) => {
   if (event.target.classList.contains("fa-trash")) {
-    const vasco = event.target.closest(".task-item")
-    console.log(vasco)
-    vasco.remove()
-    const textRemove = vasco.textContent.trim()
-    index = tasks.indexOf(textRemove)
-    tasks.splice(index, 1)
-    console.log(tasks)
+    const vasco = event.target.closest(".task-item");
+    console.log(vasco);
+    vasco.remove();
+    const textRemove = vasco.textContent.trim();
+    index = tasks.indexOf(textRemove);
+    tasks.splice(index, 1);
+    console.log(tasks);
+    if (tasks.length <= 0) {
+      btnDeleteAll.style.display = "none";
+    }
   }
-})
-
+});
 
 btnTaskAdd.onclick = function () {
-  const taskName = getTaskName();
-  taskList.innerHTML +=
-  `<section class = "task-item">
-  <p> ${taskName}</p> 
-  <i class="fa-solid fa-trash"></i>
-  </section>
-  <section class="delete-all" `;
-
-  const deleteall = 
-  console.log(tasks)
+  if (verifyInput()) {
+    const taskName = getTaskName();
+    taskList.innerHTML += `<section class = "task-item">
+    <p> ${taskName}</p> 
+    <i class="fa-solid fa-trash"></i>
+    </section>`;
+    btnDeleteAll.style.display = "flex";
+    console.log(btnDeleteAll);
+  }
 };
